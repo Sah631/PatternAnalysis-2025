cf421f2181c26de5ea891632faae3702323f14bd
Sah <sahhh0534@gmail.com>
2025-10-27 22:35:09 +1000

    Modified ADNIData class to allow validation set to also be created

--- recognition/ADNI_ConvNeXt_47219889/dataset.py
+++ recognition/ADNI_ConvNeXt_47219889/dataset.py
@@ -1,25 +1,55 @@
 import torch, os
-from torch.utils.data import Dataset
+import numpy as np
+from torch.utils.data import Dataset, Subset
 from torchvision import datasets, transforms
+
+from sklearn.model_selection import train_test_split
 
 
 class ADNIData(Dataset):
-    "Uses torchvision ImageFolder to load ADNI dataset train and test splits with transforms"
-    def __init__(self, root_dir, transform=None, split="train", seed=42):
-        self.root_dir = root_dir
-        self.transform = transform
+    """
+    Uses torchvision ImageFolder and sklearn train_test_split to load ADNI dataset
+    train, val, and test splits with appropriate transforms
+    """
+    def __init__(self, root_dir, split="train", train_transform=None, eval_transform=None, val_ratio=0.2, seed=42):
         self.split = split
-        self.seed = seed
 
-        split_dir = os.path.join(root_dir, split)
-        self.dataset = datasets.ImageFolder(split_dir, transform=transform)
+        train_dir = os.path.join(root_dir, "train")
+        test_dir  = os.path.join(root_dir, "test")
 
-        self.classes = self.dataset.classes
-        self.class_to_idx = self.dataset.class_to_idx
+        if split == "test":
+            base = datasets.ImageFolder(test_dir, transform=eval_transform)
+            self.dataset = base
+            self.classes = base.classes
+            self.class_to_idx = base.class_to_idx
+            return
+
+        base = datasets.ImageFolder(train_dir, transform=None)
+        self.classes = base.classes
+        self.class_to_idx = base.class_to_idx
+
+        targets = np.array(base.targets)
+        idx = np.arange(len(base.samples))
+
+        train_idx, val_idx = train_test_split(
+            idx,
+            test_size=val_ratio,
+            shuffle=True,
+            random_state=seed,
+            stratify=targets
+        )
+
+        base_train = datasets.ImageFolder(train_dir, transform=train_transform)
+        base_val = datasets.ImageFolder(train_dir, transform=eval_transform)
+
+        if split == "train":
+            self.dataset = Subset(base_train, train_idx)
+        else:
+            self.dataset = Subset(base_val, val_idx)
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, i):
         return self.dataset[i]
-        
+    
